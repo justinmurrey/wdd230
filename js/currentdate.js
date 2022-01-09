683725407ba3635ae8d00946fb4b61bc44ba6cd6
justinmurrey <mur19033@byui.edu>
2022-01-08 19:20:56 -0500

    commit10

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,8 +1,1 @@
-const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-};
-
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+document.getElementById("lastModified").innerHTML = new Date(document.lastModified);
