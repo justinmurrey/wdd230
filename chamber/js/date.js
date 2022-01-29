eeb6704f3713fe64ddf08a5fa10322b84be0db9b
justinmurrey <mur19033@byui.edu>
2022-01-29 13:18:31 -0500

    commit23

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -1,2 +1,17 @@
-let today = new Date();
-document.getElementById('displayDateTime').innerHTML=today;
+// select the elements to manipulate (output to)
+/*const datefield = document.querySelector("date");*/
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+/*datefield.innerHTML = `<em>${fulldate}</em>`;*/
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
