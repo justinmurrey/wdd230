e1112de5fcd6d208cbe171df0797afb5838aedb1
justinmurrey <mur19033@byui.edu>
2022-02-07 15:06:19 -0500

    COMMIT30-1

--- chamber/js/banner.js
+++ chamber/js/banner.js
@@ -1,6 +1,6 @@
 let banner = document.querySelector("#banner");
 let dayOfWeek = new Date().getDay();
 // if Monday or Tuesday displays this
-if (dayOfWeek == 2 || dayOfWeek == 3) {
+if (dayOfWeek == 1 || dayOfWeek == 2) {
   banner.style.display = "block";
 }
