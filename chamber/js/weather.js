4de2e10e07ecc5baed59e47a3c10244e9f5dc0ce
justinmurrey <mur19033@byui.edu>
2022-02-10 19:33:05 -0500

    commitit32

--- chamber/js/weather.js
+++ chamber/js/weather.js
@@ -1,20 +1,6 @@
-window.weatherWidgetConfig = window.weatherWidgetConfig || [];
-window.weatherWidgetConfig.push({
-  selector: ".weatherWidget",
-  apiKey: "29L9VL3FVUJZPDJZ39B9TWLY2",
-  location: "Zanesville, Ohio",
-  unitGroup: "us",
-  forecastDays: 3,
-  title: "Zanesvile, Ohio Weather",
-  showTitle: true,
-  showConditions: true,
-});
 
-(function () {
-  var d = document,
-    s = d.createElement("script");
-  s.src =
-    "https://www.visualcrossing.com/widgets/forecast-simple/weather-forecast-widget-simple.js";
-  s.setAttribute("data-timestamp", +new Date());
-  (d.head || d.body).appendChild(s);
-})();
+
+
+
+
+"29L9VL3FVUJZPDJZ39B9TWLY2
--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,18 +1,21 @@
-function doInputOutput() {
-  let temp = parseFloat(document.getElementsByClassName("Temp").value);
-  console.log(temp);
-  let speed = parseFloat(document.getElementById("Wind Speed").value);
-  console.log(speed);
-  let WC = windchill(temp, speed);
-  console.log(WC);
-  document.getElementById("outputDiv").innerHTML = WC;
-}
-function windchill(tempF, speed) {
-  let f =
-    35.74 +
-    0.6215 * tempF -
-    35.75 * Math.pow(speed, 0.16) +
-    0.4275 * tempF * Math.pow(speed, 0.16);
+let t = parseFloat(document.querySelector("#temp").textContent);
+let s = parseFloat(document.querySelector("#speed").textContent);
+let windchill = "";
 
-  return f;
+if (t <= 50 && s > 3) {
+  windchill = windChill(t, s);
+  windchill = `${windchill}℉`;
+} else {
+  windchill = "N/A";
+}
+// output
+document.querySelector("#wind").innerHTML = windchill;
+
+function windChill(temp, speed) {
+  return (
+    35.74 +
+    0.6215 * temp -
+    35.75 * Math.pow(speed, 0.16) +
+    0.4275 * temp * Math.pow(speed, 0.16)
+  );
 }
