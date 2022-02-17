26d1c0851fc7d45bd87cf4a4c5deb16243cabe3d
justinmurrey <mur19033@byui.edu>
2022-02-16 20:06:12 -0500

    commit37

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -12,9 +12,13 @@ if (t <= 50 && s > 3) {
 document.querySelector("#wind").innerHTML = windchill;
 
 function windChill(temp, speed) {
-  return (f =
-    35.74 +
-    0.6215 * temp -
-    35.75 * Math.pow(speed, 0.16) +
-    0.4275 * temp * Math.pow(speed, 0.16));
+  return Math.round(
+    ((f =
+      35.74 +
+      0.6215 * temp -
+      35.75 * Math.pow(speed, 0.16) +
+      0.4275 * temp * Math.pow(speed, 0.16)) *
+      100) /
+      100
+  );
 }
