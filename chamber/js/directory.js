51d94f88ac0779d87fe1696af8d161fedb8432cf
justinmurrey <mur19033@byui.edu>
2022-03-05 17:38:36 -0500

    commit46-12

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -33,4 +33,32 @@ fetch(requestURL)
 
       document.querySelector("div.cards").appendChild(card);
     }
+    .then(function (jsonObject) {
+      console.table(jsonObject); // temporary checking for valid response and data parsing
+      const companies = jsonObject["companies"];
+      for (let i = 0; i < companies.length; i++) {
+        let card = document.createElement("section");
+        let title = document.createElement("h2");
+        let address = document.createElement("p");
+        let phone = document.createElement("p");
+        let website = document.createElement("p");
+       
+  
+        title.textContent = companies[i].name;
+        address.textContent = companies[i].address;
+        phone.textContent = companies[i].phone;
+        website.textContent = companies[i].website;
+  
+        card.appendChild(title);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+        card.appendChild(image);
+  
+        image.setAttribute("src", companies[i].imageurl);
+        title.setAttribute("class", "title-dir");
+        image.setAttribute("class", "img-dir");
+  
+        document.querySelector("div.cards").appendChild(card);
+      }
   });
